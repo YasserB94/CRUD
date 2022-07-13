7fb66b2d0b452cb7836a37baf098fa7fd4c2f45b
YasserB94 <yasserbal@icloud.com>
2022-07-13 16:00:23 +0200

    Added Form for navigation trough  without router

--- Views/header.php
+++ Views/header.php
@@ -6,6 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    <form method="get" class="navigation-form">
+        <button type="submit" name="page" value="student">Student</button>
+        <button type="submit" name="page" value="teacher">Teacher</button>
+        <button type="submit" name="page" value="group">Group</button>
+    </form>
