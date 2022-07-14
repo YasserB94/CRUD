b45476e24c84ec6d6dc510230b1a27dee8257757
YasserB94 <yasserbal@icloud.com>
2022-07-14 12:27:57 +0200

    Corrected naming of USER to agreed upon variable names

--- Models/Helpers/DB_Connector.php
+++ Models/Helpers/DB_Connector.php
@@ -1,20 +1,22 @@
 <?php
-abstract class DB_Connector{
-    protected static function connect(){
+abstract class DB_Connector
+{
+    protected static function connect()
+    {
         $dbServerName = $_ENV['MYSQL_HOST'];
-        $dbUserName = $_ENV['MYSQL_USERNAME'];
+        $dbUserName = $_ENV['MYSQL_USER'];
         $dbPassword = $_ENV['MYSQL_PASSWORD'];
         $dbName = $_ENV['MYSQL_DATABASE'];
         $dsn = 'mysql:host=' . $dbServerName . ';dbname=' . $dbName . ';charset=UTF8';
-            try{
-                $pdo = new PDO($dsn, $dbUserName,$dbPassword);
-                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                if($pdo){
-                    return $pdo;
-                }
-            }catch(PDOException $error){
-                echo 'There was an error connecting to the database: ' . $dbName;
-                echo $error->getMessage();
+        try {
+            $pdo = new PDO($dsn, $dbUserName, $dbPassword);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            if ($pdo) {
+                return $pdo;
             }
+        } catch (PDOException $error) {
+            echo 'There was an error connecting to the database: ' . $dbName;
+            echo $error->getMessage();
+        }
     }
 }
