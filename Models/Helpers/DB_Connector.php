8a9850cb86a8f63c292be5fdab84fb878eb16aea
YasserB94 <yasserbal@icloud.com>
2022-07-14 10:06:11 +0200

    Added Helper Abstract class to connect to DB and retrieve a PDO object

---
+++ Models/Helpers/DB_Connector.php
@@ -0,0 +1,20 @@
+<?php
+abstract class DB_Connector{
+    protected static function connect(){
+        $dbServerName = $_ENV['MYSQL_HOST'];
+        $dbUserName = $_ENV['MYSQL_USERNAME'];
+        $dbPassword = $_ENV['MYSQL_PASSWORD'];
+        $dbName = $_ENV['MYSQL_DATABASE'];
+        $dsn = 'mysql:host=' . $dbServerName . ';dbname=' . $dbName . ';charset=UTF8';
+            try{
+                $pdo = new PDO($dsn, $dbUserName,$dbPassword);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                if($pdo){
+                    return $pdo;
+                }
+            }catch(PDOException $error){
+                echo 'There was an error connecting to the database: ' . $dbName;
+                echo $error->getMessage();
+            }
+    }
+}
