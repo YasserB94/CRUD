77cf3156b0da2a6b9768ee9fae5e46d74554a0fb
YasserB94 <yasserbal@icloud.com>
2022-07-14 15:54:56 +0200

    Split up render function

--- Controllers/GroupController.php
+++ Controllers/GroupController.php
@@ -4,6 +4,10 @@ class GroupController
 {
     public function render()
     {
+        $this->renderAllGroups();
+    }
+    private function renderAllGroups()
+    {
         $groups = DBGroupLoader::getAllGroups();
         foreach ($groups as $key => $group) {
             require __DIR__ . '../../Views/groupView.php';
