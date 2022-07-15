4040a7d31e31401470e79989e4f1811912a33039
Yasser Bal <yasserbal@icloud.com>
2022-07-15 13:17:35 +0200

    Added Edit group Functionality

--- Controllers/GroupController.php
+++ Controllers/GroupController.php
@@ -10,8 +10,11 @@ class GroupController
             $newGroupname = $_POST['addGroupName'];
             DBGroupLoader::addGroup($newGroupname);
         }
-        if(isset($_POST['editGroup'])){
-            echo $_POST['editGroup'];
+        if(isset($_POST['newGroupName'])){
+
+            $newName = $_POST['newGroupName'];
+            $idOfGroupToUpdate = $_POST['idOfGroupToUpdate'];
+            DBGroupLoader::editGroupNameByID($idOfGroupToUpdate,$newName);
         }
         if(isset($_POST['deleteGroup'])){
             $idOfGroupToDelete = $_POST['deleteGroup'];
@@ -25,6 +28,9 @@ class GroupController
             $this->renderAddButton();
         } else {
             $this->renderAddForm();
+        }
+        if(isset($_POST['editGroup'])){
+            $this->renderEditForm();
         }
     }
     private function renderAllGroups()
@@ -41,5 +47,9 @@ class GroupController
     private function renderAddForm()
     {
         require __DIR__ . '../../Views/Components/groupAddForm.php';
+    }
+    private function renderEditForm(){
+        $group = DBGroupLoader::getGroupByID($_POST['editGroup']);
+        require __DIR__ . '../../Views/Components/groupEditForm.php';
     }
 }
