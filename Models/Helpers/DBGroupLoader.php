32c262a2ea4b38178cd6722b943168e81ec367a2
Yasser Bal <yasserbal@icloud.com>
2022-07-15 13:17:55 +0200

    Added Functions to edit/get group by id

--- Models/Helpers/DBGroupLoader.php
+++ Models/Helpers/DBGroupLoader.php
@@ -12,6 +12,16 @@ abstract class DBGroupLoader extends DB_Connector
         }
         return $allgroups;
     }
+    public static function getGroupByID(int $id):Group{
+            $pdo = self::connect();
+            $sql = 'SELECT * FROM group_table WHERE id='.$id;
+            $data = $pdo->query($sql);
+            while($row=$data->fetch(PDO::FETCH_NUM)){
+                $group = new Group($row[0],$row[1]);
+                return $group;
+            }
+        
+    }
     public static function addGroup(string $groupname)
     {
         $pdo = self::connect();
@@ -22,11 +32,19 @@ abstract class DBGroupLoader extends DB_Connector
             ':name' => $groupname
         ]);
     }
+
     public static function deleteGroupByID(int $id){
         $pdo = self::connect();
         $tableName = 'group_table';
         $sql = 'DELETE FROM group_table WHERE id =' . $id;
         $statement = $pdo->prepare($sql);
         $statement->execute();
+    }
+    public static function editGroupNameByID(int $id,string $newName){
+        $pdo = self::connect();
+        $tableName = 'group_table';
+        $sql = 'UPDATE group_table SET name=? WHERE id=?';
+        $statement = $pdo->prepare($sql);
+        $statement->execute([$newName,$id]);
     }
 }
