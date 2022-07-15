0b995fd2511b4df881bb2cc9b6b428ee99a8c593
Yasser Bal <yasserbal@icloud.com>
2022-07-15 12:19:06 +0200

    On controller seleciton, Controller will check if user did delete/edit/add option

--- Controllers/GroupController.php
+++ Controllers/GroupController.php
@@ -2,10 +2,30 @@
 
 class GroupController
 {
+    public function __construct(){
+        $this->checkForOptions();
+    }
+    private function checkForOptions(){
+        if(isset($_POST['addGroupName'])){
+            $newGroupname = $_POST['addGroupName'];
+            DBGroupLoader::addGroup($newGroupname);
+        }
+        if(isset($_POST['editGroup'])){
+            echo $_POST['editGroup'];
+        }
+        if(isset($_POST['deleteGroup'])){
+            $idOfGroupToDelete = $_POST['deleteGroup'];
+            DBGroupLoader::deleteGroupByID($idOfGroupToDelete);
+        }
+    }
     public function render()
     {
         $this->renderAllGroups();
-        $this->renderAddButton();
+        if (!isset($_POST['addGroup'])) {
+            $this->renderAddButton();
+        } else {
+            $this->renderAddForm();
+        }
     }
     private function renderAllGroups()
     {
@@ -17,5 +37,9 @@ class GroupController
     private function renderAddButton()
     {
         require __DIR__ . '../../Views/Components/groupAddButton.php';
+    }
+    private function renderAddForm()
+    {
+        require __DIR__ . '../../Views/Components/groupAddForm.php';
     }
 }
