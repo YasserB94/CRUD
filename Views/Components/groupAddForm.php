7b0445ee15fc23ccb5ee0193fbb446835ebaade5
Yasser Bal <yasserbal@icloud.com>
2022-07-15 12:19:55 +0200

    Added Form to render when Add New Group option is chosen

---
+++ Views/Components/groupAddForm.php
@@ -0,0 +1,5 @@
+<form method="POST" style="display:flex;justify-content:center">
+    <label for="groupname">Groupname:</label>
+    <input type="text" name="addGroupName"></input>
+    <button type="submit">Add Group!</button>
+</form>
