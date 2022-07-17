f43cbc777c962b7045fa06cb07677cd6969dc388
CE5RIC <cedricdierckx5@hotmail.com>
2022-07-17 12:40:57 +0200

    buttons added to delete/edit teacher

--- Views/teacherView.php
+++ Views/teacherView.php
@@ -1,3 +1,7 @@
 <div>
     <span>Teacher ID: <?= $teacher->getID(); ?> </span> <span>Teacher Name: <?= $teacher->getName(); ?></span>
+    <form method="POST"><button type="submit" name="editTeacher" value="<?= $teacher->getID(); ?>">Edit this teacher</button>
+    <button type="submit" name="deleteTeacher" value="<?= $teacher->getID(); ?>">Delete</button></form>
 </div>
+
+
