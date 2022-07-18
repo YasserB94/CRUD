ac518102294e08b0642f21b22446c8172e8b9272
CE5RIC <cedricdierckx5@hotmail.com>
2022-07-18 11:34:56 +0200

    comments added to the TeacherController

--- Controllers/TeacherController.php
+++ Controllers/TeacherController.php
@@ -2,10 +2,13 @@
 
 class TeacherController
 {
+
     public function render()
     {
+        // Get all the teachers from the database
         $teachers = DBTeacherLoader::getAllTeachers();
 
+        // Loop over the all teachers and show them in the view
         foreach ($teachers as $value => $teacher) {
             require __DIR__ . '../../Views/teacherView.php';
         }
@@ -14,6 +17,8 @@ class TeacherController
       
 
 }
+
+
 
 }
 
